77eb3341be246f9deccbdd0dda1f3d07c970dc92
David Gowrie <david.gowrie@gmail.com>
2014-04-09 17:43:37 -0400

    Adding some canvas examples

--- mdn-tut/script.js
+++ mdn-tut/script.js
@@ -5,24 +5,34 @@
 	"use strict";
 
 	var canvas = document.getElementById('tutorial'),
-		c2 = document.getElementById('c2');
+		c2 = document.getElementById('c2'),
+		c3 = document.getElementById('c3');
 
 	// check for support
 	if(canvas.getContext) {
-		var cxt = canvas.getContext('2d'),
-			cxt2 = c2.getContext('2d');
+		var ctx = canvas.getContext('2d'),
+			ctx2 = c2.getContext('2d'),
+			ctx3 = c3.getContext('2d');
 		// drawing code here
 
-		cxt.fillStyle = "rgb(200,0,0)";
-		cxt.fillRect(10, 10, 55, 50);
+		ctx.fillStyle = "rgb(200,0,0)";
+		ctx.fillRect(10, 10, 55, 50);
 
-		cxt.fillStyle = "rgba(0, 0, 250, 0.5)";
-		cxt.fillRect(30, 30, 55, 50);
+		ctx.fillStyle = "rgba(0, 0, 250, 0.5)";
+		ctx.fillRect(30, 30, 55, 50);
 
 
-		cxt2.fillRect(25, 25, 100, 100);
-		cxt2.clearRect(45, 45, 60, 60);
-		cxt2.strokeRect(50, 50, 50, 50);
+		ctx2.fillRect(25, 25, 100, 100);
+		ctx2.clearRect(45, 45, 60, 60);
+		ctx2.strokeRect(50, 50, 50, 50);
+
+		// draw a triangle
+		ctx3.beginPath();
+		ctx3.moveTo(75, 50);
+		ctx3.lineTo(100, 75);
+		ctx3.lineTo(100, 25);
+		ctx3.fill();
+
 
 	} else {
 		// canvas-unsupported code here
